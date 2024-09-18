1ca274c055a8095f5fd7a79891991a34506dbde7
RomanKaptarUA <romankaptar82@gmail.com>
2024-09-18 18:05:31 +0300

    .

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,6 +4,6 @@ closeMenuBtn.addEventListener('click', toggleMenu);
 // Закрываем мобильное меню на более широких экранах
 // в случае изменения ориентации устройства.
 // Close the mobile menu on wider screens if the device orientation changes
-window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+window.matchMedia('(min-width: 768px)').addEventListener('change'), e => {
   if (!e.matches) return;
-  mobileMenu.classList.remove('is-open');
+  mobileMenu.classList.remove('is-open');}
